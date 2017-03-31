4ce9f7266d45bf2a9839ce45d950e224a1895c78
Unknown <oabbott@mail.csuchico.edu>
2017-03-30 22:26:56 -0700

    3/30/17

--- test.js
+++ test.js
@@ -5,47 +5,76 @@ function Start()
 
 function getAnswer1()
 {
- var userInput = document.getElementById("solution");
- var incorrect1;
- if (userInput != "1")
+ var userInput = document.getElementById("solution").value;
+ var incorrect = 0;
+ if (userInput != 72)
  {
-    document.getElementById("Error").innerHTML="Incorrect Answer";
-    document.getElementById("Error").classList.remove("hidden-message");
-    document.getElementById("Error").classList.add("shown-message");
-    incorrect1++;
+    alert("Incorrect Answer");
+    incorrect++;
  }
  else
  {
-   document.getElementById("solution").classList.remove("has-error");
-
+    document.getElementById("nextButton").removeAttribute("hidden");
  }
-
+ setCookie("one", incorrect, 1);
 }
+
 function getAnswer2()
 {
-
-
+  var userInput = document.getElementById("solution").value;
+  var incorrect = 0;
+  if (userInput != 45)
+  {
+     alert("Incorrect Answer");
+     incorrect++;
+  }
+  else
+  {
+     document.getElementById("nextButton").removeAttribute("hidden");
+  }
+  setCookie("two", incorrect, 1);
 }
+
 function getAnswer3()
 {
-
-
+  var userInput = document.getElementById("solution").value;
+  var incorrect = 0;
+  if (userInput != 43)
+  {
+    alert("Incorrect Answer");
+    incorrect++;
+  }
+ else
+ {
+    document.getElementById("nextButton").removeAttribute("hidden");
+ }
+ setCookie("three", incorrect, 1);
 }
+
 function nextQuestion1()
 {
-
-
+  window.location.href= "Q2.html";
 }
 
 function nextQuestion2()
 {
-
-
+  window.location.href= "Q3.html";
 }
 
 function finalPage()
 {
+  window.location.href= "results.html";
+}
 
+function results(){
+  var one = Number(getCookie("one"));
+  var two = Number(getCookie("two"));
+  var three = Number(getCookie("three"));
+  var total = Number(one + two + three);
+  document.getElementById("one").innerHTML = "Incorrect: " + one + " times";
+  document.getElementById("two").innerHTML = "Incorrect: " + two + " times";
+  document.getElementById("three").innerHTML = "Incorrect: " + three + " times";
+  document.getElementById("total").innerHTML = "Total Incorrect: " + total + " times";
 
 }
 
